93469b5d6af3d64498b4f5d2b3ecebff414d2d87
aechelsea <apiwataeinth@gmail.com>
2023-07-10 16:47:39 +0700

    Create structure of Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='text-red-600 bg-black'>
-        test
+      <div>
+        <Header />
       </div>
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <h1 className='text-center text-blue-500 border rounded-3xl border-red-700 text-2xl font-bold'>Nobel Prize</h1>
+  )
+}
+
+export default Header
